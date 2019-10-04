1d07948fe0a5d6ea4cdc4779b2aa943c4853b76b
Club de Algoritmia CUCEI <56147163+ClubAlgoritmiaCUCEI@users.noreply.github.com>
2019-10-04 11:42:33 -0500

    Update b.cpp

--- Grafos/b.cpp
+++ Grafos/b.cpp
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
-#define For(i, a, b, c) for(int i = (a); i <= (b); i += (c))
-#define Rof(i, a, b, c) for(int i = (a); i >= (b); i += (c))
-#define Forn(i, n) For(i, 0, (n) - 1, +1)
-#define For1(i, n) For(i, 1, (n), +1)
-#define Forall(i, x) for(auto i = (x).begin(); i != (x).end(); i++)
-#define size(x) (int) (x).size()
-#define all(x) (x).begin(), (x).end()
-#define _ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cout.precision(15);
-#define debug(x) cout << #x": " << x << '\n';
- 
-#define fst first
-#define snd second
-#define pb push_back
-#define lower lower_bound
-#define upper upper_bound
-#define ENDL '\n'
 using namespace std;
- 
-typedef long long lli;
-typedef unsigned long long ulli;
-typedef pair<int, int> ii;
- 
+
 const int N = 1e5 + 5;
 const int inf = 1e9;
  
